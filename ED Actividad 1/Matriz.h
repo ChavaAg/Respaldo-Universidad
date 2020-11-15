1ab39a824d4f0406218ee0c0a18c3e4772e39c9a
ChavaAg <73860157+ChavaAg@users.noreply.github.com>
2020-11-15 03:43:12 -0600

    Add files via upload

---
+++ ED Actividad 1/Matriz.h
@@ -0,0 +1,12 @@
+
+class Matriz {
+public:
+	void llenarMatriz();
+	void setCantidad(int);
+	void convercion();
+	void sumaMatriz();
+	void multiplicacion();
+	void impresion();
+private:
+	int cantidad;
+};
